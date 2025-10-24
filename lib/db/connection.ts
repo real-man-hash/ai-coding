c2663e98aa27716df8f77391ee59a5b227f0a53c
jesse.ling <jesse.ling@gaodun.com>
2025-10-24 19:10:20 +0800

    fix: new8

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -3,11 +3,11 @@ import mysql from "mysql2/promise";
 import * as schema from "./schema";
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST || "10.3.7.16",
-  port: parseInt(process.env.DB_PORT || "3306"),
-  user: process.env.DB_USER || "gdtest",
-  password: process.env.DB_PASSWORD || "gdmysql_221",
-  database: process.env.DB_NAME || "gd_bot",
+  host: process.env.MYSQL_HOST || "10.3.7.16",
+  port: parseInt(process.env.MYSQL_PORT || "3306"),
+  user: process.env.MYSQL_USERNAME || "gdtest",
+  password: process.env.MYSQL_PASSWORD || "gdmysql_221",
+  database: process.env.MYSQL_DATABASE || "gd_bot",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
