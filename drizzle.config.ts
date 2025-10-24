e60e0310eb3c932e2757f5aaa5b678861cdcd4ae
jesse <masked233@163.com>
2025-10-24 22:03:31 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,10 +5,10 @@ export default defineConfig({
   out: "./db/migrations",
   dialect: "mysql",
   dbCredentials: {
-    host: process.env.DB_HOST || "dev.mysql.gaodunwangxiao.com",
-    port: Number(process.env.DB_PORT) || 3306,
-    user: process.env.DB_USER || "gdtest",
-    password: process.env.DB_PASSWORD || "gdmysql_221",
-    database: process.env.DB_NAME || "gd_bot",
+    host: process.env.MYSQL_HOST || "dev.mysql.gaodunwangxiao.com",
+    port: Number(process.env.MYSQL_PORT) || 3306,
+    user: process.env.MYSQL_USERNAME || "gdtest",
+    password: process.env.MYSQL_PASSWORD || "gdmysql_221",
+    database: process.env.MYSQL_DATABASE || "gd_bot",
   },
 });
